b5d387fed1c5e02c691c4be82e167ef2fb4a2f93
Wallace Wan <wallace.wan@sap.com>
2016-04-05 13:44:16 +0800

    Desc: Encapsulate the internalframe

---
+++ src/app/provider/CalcProvider.java
@@ -0,0 +1,14 @@
+/**
+ * 
+ */
+package app.provider;
+
+import javax.swing.JInternalFrame;
+
+/**
+ * @author I310818
+ *
+ */
+public interface CalcProvider {
+    JInternalFrame createInternalFrame();
+}
